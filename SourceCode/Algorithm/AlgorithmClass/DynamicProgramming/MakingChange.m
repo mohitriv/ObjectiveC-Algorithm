868903b291d2e1be8ed14e2fa2f103541d281a2f
Mohit Trivedi <trivedi.moh@gmail.com>
2015-05-19 18:29:58 +0530

    Implementation of making change algorithm

--- SourceCode/Algorithm/AlgorithmClass/DynamicProgramming/MakingChange.m
+++ SourceCode/Algorithm/AlgorithmClass/DynamicProgramming/MakingChange.m
@@ -13,7 +13,7 @@
 -(void) runTheAlgorithm {
     // Define the input
     NSArray *coinsArr = [NSArray arrayWithObjects:[NSNumber numberWithInt:1],[NSNumber numberWithInt:2],[NSNumber numberWithInt:5],[NSNumber numberWithInt:10],[NSNumber numberWithInt:20],[NSNumber numberWithInt:50], nil];
-    NSInteger changeMoney = 4;
+    NSInteger changeMoney = 57;
     NSInteger minCoins = [self findMinCoinsRequiredToMakeChangeforMoney:changeMoney fromCoinsArr:coinsArr];
     [super printOutput:[NSString stringWithFormat:@"Minimum coins required for making change for money %@ from array %@ are %@",[NSNumber numberWithInteger:changeMoney],coinsArr,[NSNumber numberWithInteger:minCoins]]];
 }
@@ -28,13 +28,14 @@
             [minArr addObject:[NSNumber numberWithInteger:0]];
             continue;
         }
-        for (int j=0; j<i; j++) {
+        for (int j=0; j<[coinsArr count]; j++) {
             NSInteger index = i-[coinsArr[j] integerValue];
             if (index<0) {
                 break;
             }
-            if (index<=min) {
-                min = index;
+            NSInteger val = [[minArr objectAtIndex:index] integerValue];
+            if (val<=min) {
+                min = val;
             }
         }
         [minArr addObject:[NSNumber numberWithInteger:min+1]];
