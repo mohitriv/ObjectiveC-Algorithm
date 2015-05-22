e5a8704555ebd077248df3f70eea259bc1ff49e6
Mohit Trivedi <trivedi.moh@gmail.com>
2015-05-22 16:05:34 +0530

    Implemented the Longest Increasing Subsequence

--- SourceCode/Algorithm/AlgorithmClass/DynamicProgramming/LongestIncreasingSubsequece.m
+++ SourceCode/Algorithm/AlgorithmClass/DynamicProgramming/LongestIncreasingSubsequece.m
@@ -12,13 +12,37 @@
 
 -(void) runTheAlgorithm {
     // Define the input
-    NSArray *inputArr = [NSArray arrayWithObjects:[NSNumber numberWithInt:20],[NSNumber numberWithInt:30],[NSNumber numberWithInt:40],[NSNumber numberWithInt:20],[NSNumber numberWithInt:30],[NSNumber numberWithInt:10], nil];
-    //[super printOutput:[NSString stringWithFormat:@"Maximum sum for subsequence of the sequence %@ is %@",inputArr,[NSNumber numberWithInteger:maxSum]]];
+    NSArray *inputArr = [NSArray arrayWithObjects:[NSNumber numberWithInt:20],[NSNumber numberWithInt:30],[NSNumber numberWithInt:10],[NSNumber numberWithInt:15],[NSNumber numberWithInt:50],[NSNumber numberWithInt:50], nil];
+    NSInteger longestLength = [self findLongestIncreasingSubsequence:inputArr];
+    [super printOutput:[NSString stringWithFormat:@"Longest increasing subsequence of the sequence %@ is %@",inputArr,[NSNumber numberWithInteger:longestLength]]];
+}
+
+-(NSInteger) findLongestIncreasingSubsequence :(NSArray*) inputArr {
+    NSMutableArray *tempArr = [[NSMutableArray alloc] init];
+    // L(0) = 0
+    for (int j=0; j<[inputArr count]; j++) {
+        NSInteger maxVal = 0;
+        for (int i=0; i<j; i++) {
+            if ([[inputArr objectAtIndex:i] integerValue] >= [[inputArr objectAtIndex:j] integerValue]) {
+                // Do nothing
+            }
+            else {
+                maxVal = MAX(maxVal, [[tempArr objectAtIndex:i] integerValue]);
+            }
+            
+        }
+        [tempArr addObject:[NSNumber numberWithInteger:maxVal+1]];
+    }
+    // Max(L(j))
+    NSInteger max = 0;
+    for (int i=0; i<[tempArr count]; i++) {
+        max = MAX(max, [[tempArr objectAtIndex:i] integerValue]);
+    }
+    return max;
 }
 
 -(NSString*) getTimeComplexity {
     return [NSString stringWithFormat:@"O(n*n)"];
 }
-
 
 @end
